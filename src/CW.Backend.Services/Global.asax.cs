1772f72cb515f4444311404dd08cb148fe4a58ea
sheikh-imrul <sheikh.imrul@gmail.com>
2015-01-29 20:37:08 +0600

    API Help Page added

--- src/CW.Backend.Services/Global.asax.cs
+++ src/CW.Backend.Services/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace CW.Backend.Services
@@ -11,6 +12,8 @@ namespace CW.Backend.Services
     {
         protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
